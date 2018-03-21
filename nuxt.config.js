1d584e0cc35d48d726a252bb220c65e5cad10716
yamanoku <0910yama@gmail.com>
2018-03-21 20:33:42 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Okuto Oyama Portfolio Site.' },
-      { hid: 'og:image', name: 'og:image', content: '/iam.jpg' },
+      { hid: 'og:image', name: 'og:image', content: 'https://yamanoku.net/iam.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
