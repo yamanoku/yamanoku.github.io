c3ba6a4c5d553952abec533967b67210bfbbe4b3
yamanoku <0910yama@gmail.com>
2018-04-18 00:29:51 +0900

    fix NewsList.vue
    fix no title response
    
    loading components
    
    fix pagination styles

--- components/NewsList.vue
+++ components/NewsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="shown">
     <paginate-links
       for="lists"
       :limit="2"
@@ -9,10 +9,14 @@
     <paginate
       tag="ul"
       name="lists"
+      class="lists"
       :list="lists"
       :per="5">
-      <li v-for="list in paginated('lists')" :key="list.iid" v-if="list.attachments[0].title">
-        <a :href="list.attachments[0].title_link" target="_blank" rel="noopener">{{ list.attachments[0].title }}</a>
+      <li v-for="list in paginated('lists')" :key="list.iid">
+        <!-- title not response -->
+        <a v-if="!list.attachments[0].title" :href="urlRender(list.attachments[0].text)" target="_blank" rel="noopener">{{ textRender(list.attachments[0].text) }}</a>
+        <!-- title response -->
+        <a v-else :href="list.attachments[0].title_link" target="_blank" rel="noopener">{{ list.attachments[0].title }}</a>
       </li>
     </paginate>
     <paginate-links
@@ -21,6 +25,13 @@
       :show-step-links="true"
       :async="true">
     </paginate-links>
+  </div>
+  <div v-else>
+    <ul class="loading">
+      <li v-for="i in 5" :key="i">
+        ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -32,16 +43,38 @@
       return {
         lists: [],
         paginate: ['lists'],
+        shown: false
       }
     },
     mounted() {
       axios.get(TOKEN)
         .then((res) => {
           this.lists = res.data.messages.matches;
+          this.shown = true;
         });
+    },
+    methods: {
+      textRender (text) {
+        const mystr = text.split("Reading... ").join("");
+        return mystr.replace(/^<[^>]h>|<[^>]*>$/g, "");
+      },
+      urlRender (text) {
+        const mystr = text.split("Reading... ").join("");
+        return mystr.match(/ <([^\s]+)/)[1].slice(0, -1);
+      }
     }
   }
 </script>
+
+<style scoped>
+.loading {
+  padding: 32px 0;
+}
+.loading li {
+  letter-spacing: -5px;
+  overflow-x: hidden;
+}
+</style>
 
 <style>
 .paginate-links {
@@ -60,22 +93,21 @@
   line-height: 32px;
   font-weight: 700;
   color: #3a2c09;
-  margin-right: 5px;
+  margin-right: 2px;
 }
 .paginate-links li a {
   display: block;
   width: 100%;
   height: 100%;
   color: #3a2c09;
-  border-radius: 4px;
   cursor: pointer;
 }
 .paginate-links li a:hover,
 .paginate-links li.active a,
 .paginate-links li.left-arrow a,
 .paginate-links li.right-arrow a {
-  color: #3a2c09;
-  background-color: #ebe9e6;
+  color: #fff;
+  background-color: #37455d;
   opacity: 1;
 }
 .paginate-links li.right-arrow {
