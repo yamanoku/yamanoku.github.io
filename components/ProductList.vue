4b39f42ed074cc5f603f7e33fdc17dd0865d73ae
yamanoku <0910yama@gmail.com>
2018-05-21 09:53:20 +0900

    revise
    revise product url

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,8 +1,8 @@
 <template>
   <ul>
-    <li><a href="/reading">Reading...</a></li>
+    <li><a href="https://reading.yamanoku.net/" target="_blank" rel="noopener">Reading...</a></li>
     <li><a href="https://vue-portfolio.yamanoku.net/" target="_blank" rel="noopener">Vue Portfolio Templete</a></li>
-    <li><a href="/birthday-countdown-js" target="_blank" rel="noopener">Birthday Countdown</a></li>
+    <li><a href="/birthday-countdown-js">Birthday Countdown</a></li>
     <li><a href="/glicthrange">glicthrange</a></li>
     <li><a href="https://github.com/yamanoku/Qiita_Code-Style_Before" target="_blank" rel="noopener">Qiita Code Style Before</a></li>
   </ul>
