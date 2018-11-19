28a2226aa6f889dc560bdad7f8530514e671ba34
yamanoku <0910yama@gmail.com>
2018-11-20 00:32:43 +0900

    update product

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,5 +1,6 @@
 <template>
   <ul>
+    <li><a href="https://booth.pm/ja/items/1044446" target="_blank" rel="noopener">Web Accessibility for Beginners.</a></li>
     <li><a href="https://www.npmjs.com/package/yamanoku">yamanoku CLI</a></li>
     <li><a href="https://reading.yamanoku.net">Reading...</a></li>
     <li><a href="https://vue-portfolio.yamanoku.net/" target="_blank" rel="noopener">Vue Portfolio Templete</a></li>
