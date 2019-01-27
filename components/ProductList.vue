1e19a23cfd1b98e39b3c77a7727efc93e5b5fb65
yamanoku <0910yama@gmail.com>
2019-01-27 22:39:52 +0900

    add product

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,17 +1,47 @@
 <template>
   <ul>
     <li>
-      Web Accessibility for Beginners.
+      <a
+        href="https://github.com/yamanoku/slack-reaction-primoji"
+        target="_blank"
+        rel="noopener"
+      >slack-reaction-primoji</a>
+    </li>
+    <li>Web Accessibility for Beginners.
       <ul>
-        <li><a href="https://booth.pm/ja/items/1044446" target="_blank" rel="noopener">booth</a></li>
-        <li><a href="https://note.mu/yamanoku/n/n3487a344ff84" target="_blank" rel="noopener">note</a></li>
+        <li>
+          <a href="https://booth.pm/ja/items/1044446" target="_blank" rel="noopener">booth</a>
+        </li>
+        <li>
+          <a href="https://note.mu/yamanoku/n/n3487a344ff84" target="_blank" rel="noopener">note</a>
+        </li>
       </ul>
     </li>
-    <li><a href="https://www.npmjs.com/package/yamanoku">yamanoku CLI</a></li>
-    <li><a href="https://reading.yamanoku.net">Reading...</a></li>
-    <li><a href="https://vue-portfolio.yamanoku.net/" target="_blank" rel="noopener">Vue Portfolio Templete</a></li>
-    <li><a href="/birthday-countdown-js">Birthday Countdown</a></li>
-    <li><a href="/glicthrange">glicthrange</a></li>
-    <li><a href="https://github.com/yamanoku/Qiita_Code-Style_Before" target="_blank" rel="noopener">Qiita Code Style Before</a></li>
+    <li>
+      <a href="https://www.npmjs.com/package/yamanoku">yamanoku CLI</a>
+    </li>
+    <li>
+      <a href="https://reading.yamanoku.net">Reading...</a>
+    </li>
+    <li>
+      <a
+        href="https://vue-portfolio.yamanoku.net/"
+        target="_blank"
+        rel="noopener"
+      >Vue Portfolio Templete</a>
+    </li>
+    <li>
+      <a href="/birthday-countdown-js">Birthday Countdown</a>
+    </li>
+    <li>
+      <a href="/glicthrange">glicthrange</a>
+    </li>
+    <li>
+      <a
+        href="https://github.com/yamanoku/Qiita_Code-Style_Before"
+        target="_blank"
+        rel="noopener"
+      >Qiita Code Style Before</a>
+    </li>
   </ul>
 </template>
