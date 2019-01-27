a3f4939ea23fb5b6394666443149faa24f5c5c92
yamanoku <0910yama@gmail.com>
2019-01-28 00:35:56 +0900

    fix vue-a11y
    vue-a11y/accessible-emoji

--- components/BasicInfo.vue
+++ components/BasicInfo.vue
@@ -2,11 +2,10 @@
   <ul>
     <li>1989/10/30 Born in Noshiro, Akita, Japan</li>
     <li>Sex - Man</li>
-    <li>
-      2008 Get into <a href="http://www.iwate-u.ac.jp/" target="_blank" rel="noopener">Iwate University</a> of Art and Culture Course.
+    <li>2008 Get into
+      <a href="http://www.iwate-u.ac.jp/" target="_blank" rel="noopener">Iwate University</a> of Art and Culture Course.
       <ul>
-        <li>
-          Bachelor of  Art and Culture Course.（BFA）
+        <li>Bachelor of Art and Culture Course.（BFA）
           <ul>
             <li>Fine Arts</li>
             <li>Engineering and Design</li>
@@ -16,7 +15,12 @@
     </li>
     <li>2014 Get married.</li>
     <li>2016 Daughter is born</li>
-    <li>👨‍👩‍👧 &amp; 🐈🐈🐈</li>
+    <li>
+      <span role="img" aria-label="Family With Girl">👨‍👩‍👧</span> &amp;
+      <span role="img" aria-label="cat">🐈</span>
+      <span role="img" aria-label="cat">🐈</span>
+      <span role="img" aria-label="cat">🐈</span>
+    </li>
     <li>Live in Kamagaya, Chiba, Japan</li>
   </ul>
 </template>
