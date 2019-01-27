8512eb124df063b380eed51da3c4d151a5ba83cc
yamanoku <0910yama@gmail.com>
2019-01-28 00:45:06 +0900

    fix axe check
    heading1 svg role=img

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,9 @@
 <template>
     <header>
       <h1>
-        <svg xmlns="http://www.w3.org/2000/svg" width="246" height="242" viewBox="0 0 246 242">
-          <path id="icon_white.svg" class="cls-1" d="M64,67v54l82,82-46,46v60h56L310,155V96H230l-21,20L160,67H64ZM176,203l-45,46h25L293,113H230l-39,39-31-31H94Z" transform="translate(-64 -67)"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="246" height="242" viewBox="0 0 246 242" role="img" aria-labelledby="title">
+          <title id="title">yamanoku</title>
+          <path class="cls-1" d="M64,67v54l82,82-46,46v60h56L310,155V96H230l-21,20L160,67H64ZM176,203l-45,46h25L293,113H230l-39,39-31-31H94Z" transform="translate(-64 -67)"/>
         </svg>
       </h1>
     </header>
