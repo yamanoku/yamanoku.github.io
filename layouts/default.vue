6193ed1b771e8087c7128f2f298514306d0e7b25
yamanoku <0910yama@gmail.com>
2019-08-04 22:42:44 +0900

    del base-line

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,28 +31,6 @@ html {
 *:after {
   box-sizing: border-box;
 }
-body {
-    position: relative;
-}
-body:active:after {
-    display: none;
-}
-body:after {
-    background: linear-gradient(to bottom, rgba(200,225,255,0.6) 1px, transparent 1px);
-    background-size: 17px 25px;
-    bottom: 0;
-    margin-top: 0px;
-    content: '';
-    display: block;
-    height: auto;
-    left: 0;
-    pointer-events: none;
-    position: absolute;
-    right: 0;
-    top: -1px;
-    width: auto;
-    z-index: 9999;
-}
 @media (prefers-color-scheme: dark) {
   html {
     background-color: var(--black);
