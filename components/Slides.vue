fed12b6a9a2b8ec6a8b62d0e6dbf5d00198f6b1d
yamanoku <0910yama@gmail.com>
2019-08-11 21:39:21 +0900

    add anchor-link

--- components/Slides.vue
+++ components/Slides.vue
@@ -3,7 +3,17 @@
     <template v-if="this.$i18n.locale !== 'ja'">
       <em>{{ $t("onlyJPText") }}</em>
     </template>
-    <h4>{{ $t("heading.slides.stage") }}</h4>
+    <h4 id="slides-stage">
+      <a class="anchor" aria-hidden="true" href="#slides-stage">
+        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          />
+        </svg>
+      </a>
+      {{ $t("heading.slides.stage") }}
+    </h4>
     <ul role="list">
       <li role="listitem">
         <a
@@ -48,7 +58,17 @@
         >stylelint-a11yについて調べてわかったこと</a>
       </li>
     </ul>
-    <h4>{{ $t("heading.slides.study") }}</h4>
+    <h4 id="slides-study">
+      <a class="anchor" aria-hidden="true" href="#slides-study">
+        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          />
+        </svg>
+      </a>
+      {{ $t("heading.slides.study") }}
+    </h4>
     <ul role="list">
       <li role="listitem">
         <a
@@ -78,7 +98,17 @@
         </ul>
       </li>
     </ul>
-    <h4>{{ $t("heading.slides.write") }}</h4>
+    <h4 id="slides-write">
+      <a class="anchor" aria-hidden="true" href="#slides-write">
+        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          />
+        </svg>
+      </a>
+      {{ $t("heading.slides.write") }}
+    </h4>
     <ul role="list">
       <li role="listitem">
         <a
@@ -116,7 +146,17 @@
         >PlayBackTech2018</a>
       </li>
     </ul>
-    <h4>{{ $t("heading.slides.lt") }}</h4>
+    <h4 id="slides-lt">
+      <a class="anchor" aria-hidden="true" href="#slides-lt">
+        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+          <path
+            fill-rule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          />
+        </svg>
+      </a>
+      {{ $t("heading.slides.lt") }}
+    </h4>
     <ul role="list">
       <li role="listitem">
         <a href="/LT/lt01/" target="_blank" rel="noopener">猫のすすめ</a>
--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,131 @@
         {{ $t("name") }}
       </h2>
       <h2 v-else>{{ $t("name") }}</h2>
-      <h3>{{ $t("heading.job") }}</h3>
+      <h3 id="job">
+        <a class="anchor" aria-hidden="true" href="#job">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.job") }}
+      </h3>
       <job-info-component />
-      <h3>{{ $t("heading.basic") }}</h3>
+      <h3 id="basic">
+        <a class="anchor" aria-hidden="true" href="#basic">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.basic") }}
+      </h3>
       <basic-info-component />
-      <h3>{{ $t("heading.career") }}</h3>
+      <h3 id="career">
+        <a class="anchor" aria-hidden="true" href="#career">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.career") }}
+      </h3>
       <career-info-component />
-      <h3>{{ $t("heading.productList") }}</h3>
+      <h3 id="product">
+        <a class="anchor" aria-hidden="true" href="#product">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.productList") }}
+      </h3>
       <product-list-component />
-      <h3>{{ $t("heading.slides.title") }}</h3>
+      <h3 id="slide">
+        <a class="anchor" aria-hidden="true" href="#slide">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.slides.title") }}
+      </h3>
       <slides-component />
-      <h3>{{ $t("heading.sns") }}</h3>
+      <h3 id="sns">
+        <a class="anchor" aria-hidden="true" href="#sns">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.sns") }}
+      </h3>
       <sns-component />
-      <h3>{{ $t("heading.contact") }}</h3>
+      <h3 id="contact">
+        <a class="anchor" aria-hidden="true" href="#contact">
+          <svg
+            class="octicon octicon-link"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+            />
+          </svg>
+        </a>
+        {{ $t("heading.contact") }}
+      </h3>
       <address-component />
     </main>
     <nav>
@@ -88,6 +200,7 @@ h3 {
   font-feature-settings: "palt";
   letter-spacing: 0.0125em;
   line-height: var(--rhythm);
+  position: relative;
 }
 h4 {
   margin: var(--rhythm) 0;
@@ -95,6 +208,24 @@ h4 {
   font-feature-settings: "palt";
   letter-spacing: 0.0125em;
   line-height: var(--rhythm);
+  position: relative;
+}
+.anchor {
+  position: absolute;
+  left: -24px;
+}
+@media screen and (max-width: 768px) {
+  .anchor {
+    display: none;
+  }
+}
+.anchor path {
+  fill: rgba(0, 0, 0, 0.6);
+}
+@media (prefers-color-scheme: dark) {
+  .anchor path {
+    fill: var(--blue);
+  }
 }
 ul:not([class]) {
   margin: var(--rhythm) 0;
