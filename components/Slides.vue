ebfe377036490d5f7e4beb2f88d5f26a93a9697f
yamanoku <0910yama@gmail.com>
2019-08-12 09:46:04 +0900

    add Common Icons

--- components/Slides.vue
+++ components/Slides.vue
@@ -5,12 +5,7 @@
     </template>
     <h4 id="slides-stage">
       <a class="anchor" aria-hidden="true" href="#slides-stage">
-        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-          <path
-            fill-rule="evenodd"
-            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-          />
-        </svg>
+        <anchor-icon />
       </a>
       {{ $t("heading.slides.stage") }}
     </h4>
@@ -60,12 +55,7 @@
     </ul>
     <h4 id="slides-study">
       <a class="anchor" aria-hidden="true" href="#slides-study">
-        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-          <path
-            fill-rule="evenodd"
-            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-          />
-        </svg>
+        <anchor-icon />
       </a>
       {{ $t("heading.slides.study") }}
     </h4>
@@ -100,12 +90,7 @@
     </ul>
     <h4 id="slides-write">
       <a class="anchor" aria-hidden="true" href="#slides-write">
-        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-          <path
-            fill-rule="evenodd"
-            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-          />
-        </svg>
+        <anchor-icon />
       </a>
       {{ $t("heading.slides.write") }}
     </h4>
@@ -148,12 +133,7 @@
     </ul>
     <h4 id="slides-lt">
       <a class="anchor" aria-hidden="true" href="#slides-lt">
-        <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-          <path
-            fill-rule="evenodd"
-            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-          />
-        </svg>
+        <anchor-icon />
       </a>
       {{ $t("heading.slides.lt") }}
     </h4>
---
+++ plugins/icons.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import GithubIcon from "~/assets/svg-icons/GithubIcon.vue";
+import NpmIcon from "~/assets/svg-icons/NpmIcon.vue";
+import AnchorIcon from "~/assets/svg-icons/AnchorIcon.vue";
+
+Vue.component("github-icon", GithubIcon);
+Vue.component("npm-icon", NpmIcon);
+Vue.component("anchor-icon", AnchorIcon);
