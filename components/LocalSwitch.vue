4d7f2247ab7b9304ed52fea594e1ad07bdd7d74e
yamanoku <0910yama@gmail.com>
2019-08-15 22:45:01 +0900

    Setting lang attr

--- components/LocalSwitch.vue
+++ components/LocalSwitch.vue
@@ -3,7 +3,7 @@
     <h3>{{ $t("multilingual")}}</h3>
     <ul role="list">
       <li role="listitem" v-for="locale in $i18n.locales" :key="locale.code">
-        <nuxt-link :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
+        <nuxt-link :lang="locale.code" :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
       </li>
     </ul>
   </nav>
