df4389f444324bbcacccf327e8fd65c788bbffbf
yamanoku <0910yama@gmail.com>
2019-08-27 21:28:48 +0900

    update

--- pages/about.vue
+++ pages/about.vue
@@ -37,9 +37,9 @@
         </a>
         マークアップ
       </h4>
-      <p>ヒューマンリーダブル（人が理解できる）とマシンリーダブル（機械が理解できる）な情報設計を実現するためにセマンティクスなマークアップ実装しています。</p>
-      <p>支援技術を使用する人には不要となる要素について、WAI-ARIA（Web Accessibility Initiative - Accessible Rich Internet Applications）を使用した実装も行っています。</p>
-      <highlight-code lang="html">{{waiAria}}</highlight-code>
+      <p>ヒューマンリーダブル（人が理解できる）とマシンリーダブル（機械が理解できる）となる情報設計を実現するためにセマンティクスなマークアップ実装しています。</p>
+      <p>支援技術を使用する人には不要となる要素について、WAI-ARIA（Web Accessibility Initiative - Accessible Rich Internet Applications）を使用して閲覧を阻害しないようにする実装も行っています。</p>
+      <highlight-code id="wai-aria-in-html" lang="html">{{waiAria}}</highlight-code>
       <h4 id="component-oriented">
         <a class="anchor" aria-hidden="true" href="#component-oriented">
           <anchor-icon />
@@ -52,17 +52,49 @@
           :to="localePath({ name: 'index', hash: '#slides'})"
         >{{$t("heading.slides.title")}}</nuxt-link>のリストでは以下コンポーネントを使ってレンダリングしています。
       </p>
-      <highlight-code lang="vue">{{slide}}</highlight-code>
-      <p>取得するAPIの整形は必要ですが、１つのコンポーネントで管理することで、ソースコードの簡略化・複数での使い回しや情報設計を効率化することができます。</p>
-      <p>その他、コンポーネント指向開発でPropsが深くなってしまわないようにstoreから各APIを呼び出すように設定しています。</p>
-      <highlight-code lang="javascript">{{api}}</highlight-code>
+      <highlight-code id="slide-components" lang="vue">{{slide}}</highlight-code>
+      <ul role="list">
+        <li role="listitem">
+          <a href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/slides/SlideList.vue" target="_blank" rel="noopener">
+            yamanoku.github.io/SlideList.vue
+            <github-icon />
+          </a>
+        </li>
+      </ul>
+      <p>得るためのAPIの整形は必要ですが、1つのコンポーネントで管理することで、ソースコードの簡略化・複数での使い回しや情報設計の効率化を図ることができます。</p>
+      <p>その他、コンポーネント指向開発でPropsが深くなってしまわないように、Storeから各APIを呼び出すように設定しています。</p>
+      <highlight-code id="api-for-store" lang="javascript">{{api}}</highlight-code>
+      <ul role="list">
+        <li role="listitem">
+          <a href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/products/A11yBeginner.vue" target="_blank" rel="noopener">
+            yamanoku.github.io/A11yBeginner.vue
+            <github-icon />
+          </a>
+        </li>
+      </ul>
+      <h4 id="pwa">
+        <a class="anchor" aria-hidden="true" href="#pwa">
+          <anchor-icon />
+        </a>
+        PWA (Progressive Web Application)
+      </h4>
+      <p>Google Chromeで閲覧している場合、PCからはデスクトップPWA、モバイル端末からはPWAとして利用することができます。</p>
+      <p>ブラウザを通さずにネイティブアプリとして起動できたり、キャッシュを活用した早いページ体験、1度ダウンロードするとオフラインで閲覧することもできます。</p>
+      <ul>
+        <li>
+          <a href="https://developers.google.com/web/fundamentals/codelabs/your-first-pwapp/" target="_blank" rel="noopener">
+            Your First Progressive Web App  |  Web Fundamentals  |  Google Developers
+          </a>
+        </li>
+      </ul>
       <h4 id="i18n">
         <a class="anchor" aria-hidden="true" href="#i18n">
           <anchor-icon />
         </a>
         国際化対応
       </h4>
-      <p>Nuxt.jsで開発しているのでnuxt-18nというツールを入れています。日本語と英語のほか、その他の言語対応もしていこうと思っています。</p>
+      <p>Nuxt.jsで開発しているのでnuxt-18nというツールを入れています。国際化対応ほか、特定の言語のみに表示する場合などのレンダリング処理も可能となっています。</p>
+      <p>日本語と英語を標準で対応しておりますが、その他の言語対応もしていこうと思っています。</p>
       <ul role="list">
         <li role="listitem" v-for="locale in $i18n.locales" :key="locale.code">
           <nuxt-link :lang="locale.code" :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
@@ -75,13 +107,23 @@
         ページデザインの解説
       </h3>
       <p>あらゆる層の方が見やすいようにインクルーシブデザインを取り入れています。</p>
+      <h4>オーバーデザインしすぎない</h4>
+      <p>過剰な装飾をしすぎることは目的の情報にたどり着く阻害に成り得ます。ブラウザのデフォルトスタイル、つまり標準の姿を意識したデザインにしています。</p>
+      <p>また、セマンティクスなマークアップで設計していることで、スタイルを外してもデフォルトスタイルのままでも違和感なく情報にたどり着けるようにも繋がります。</p>
+      <ul role="list">
+        <li role="listitem">
+          <a href="//motherfuckingwebsite.com/" target="_blank" rel="noopener">
+            This is a motherfucking website.
+          </a>
+        </li>
+      </ul>
       <h4 id="font-family">
         <a class="anchor" aria-hidden="true" href="#font-size">
           <anchor-icon />
         </a>
-        フォントについて
+        日本語フォントについて
       </h4>
-      <p>日本語UDフォント<a href="https://minmoji.ucda.jp/pages/webfont/" target="_blank" rel="noopener">みんなの文字&reg;WEBフォント</a>を使用しています。</p>
+      <p>UDフォントである<a href="https://minmoji.ucda.jp/pages/webfont/" target="_blank" rel="noopener">みんなの文字&reg;WEBフォント</a>を使用しています。</p>
       <h4 id="font-size">
         <a class="anchor" aria-hidden="true" href="#font-size">
           <anchor-icon />
@@ -95,7 +137,8 @@
         </a>
         カラーコントラスト
       </h4>
-      <p>使用しているカラーはいずれも、背景とのコントラスト比が最低4.5:1となるように設定されていますが、純粋な白黒ではなく、濃淡を抑えてバランスをとったカラーとなっています。CSSカスタム・プロパティ（CSS変数）を使用して指定しています。</p>
+      <p>使用しているカラーはいずれも、背景とのコントラスト比が最低でも4.5:1となるように設定されていますが、純粋な白黒ではなく、濃淡を抑えてバランスをとったカラーとなっています。</p>
+      <p>共通で使用できるよう、以下カスタム・プロパティ（CSS変数）を使用して指定しています。</p>
       <dl>
         <div v-for="color in colors" :key="color.name" :style="'background-color:' + color.value">
           <dt :style="(color.name === 'var(--black)' || color.name === 'var(--linkBlue)' ? 'color: #fff': '')">{{color.name}}</dt>
@@ -214,6 +257,11 @@ dl {
   display: grid;
   background-color: #fff;
   grid-template-columns: repeat(3, 1fr);
+}
+@media screen and (max-width: 768px) {
+  dl {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 dl > div {
   padding: calc(var(--rhythm) / 2);
