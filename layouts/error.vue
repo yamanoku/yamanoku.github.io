b26e46ff4f915f05de632edc94c1650bc531d989
yamanoku <0910yama@gmail.com>
2019-09-03 08:36:00 +0900

    init page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <h2>{{ error.statusCode }}</h2>
+    <p v-if="error.statusCode === 404">ページが見つかりません</p>
+    <p v-else>エラーが発生しました</p>
+    <nuxt-link :to="localePath({ name: 'index'})">{{$t("backTop")}}</nuxt-link>
+  </main>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500;
+    }
+  }
+};
+</script>
