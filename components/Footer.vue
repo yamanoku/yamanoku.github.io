69ce8702e421d69de4321165abdb83fb85e8483f
yamanoku <0910yama@gmail.com>
2019-09-03 23:09:31 +0900

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <small>&copy; Copyright {{currentYear}}, Okuto Oyama</small>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: this.$store.state.day.currentYear,
+    };
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  max-width: 80ch;
+  margin: auto;
+  padding: var(--rhythm);
+  text-align: center;
+}
+small {
+  line-height: var(--rhythm);
+}
+</style>
