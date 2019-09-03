9f3aa8bc470908802b544f1133ae694ff55e3ecb
yamanoku <0910yama@gmail.com>
2019-09-04 00:14:47 +0900

    add new ogp img

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,13 +14,13 @@ module.exports = {
       {
         hid: "og:image",
         name: "og:image",
-        content: "https://yamanoku.net/iam.jpg"
+        content: "https://yamanoku.net/ogp@2x.png"
       },
       { name: "twitter:card", content: "summary" },
       { name: "twitter:site", content: "@yamanoku" },
       { name: "twitter:title", content: "yamanoku.net" },
       { name: "twitter:description", content: "Okuto Oyama Portfolio Site" },
-      { name: "twitter:image", content: "https://yamanoku.net/iam.jpg" },
+      { name: "twitter:image", content: "https://yamanoku.net/ogp@2x.png" },
       { name: "twitter:image:alt", content: "Portrait for Okuto Oyama" }
     ],
     link: [
