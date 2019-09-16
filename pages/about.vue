58d7e8adac37d23450380fedb6c324f6ebe9be1f
yamanoku <0910yama@gmail.com>
2019-09-16 16:50:52 +0900

    fix about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -98,7 +98,7 @@ export default {
     return {
       meta: {
         title: this.$t("aboutPage.title"),
-        description: this.$t("aboutPage.description")
+        description: this.$t("aboutPage.description"),
         image: "https://yamanoku.net/ogp/ogp-about@2x.png"
       },
     }
