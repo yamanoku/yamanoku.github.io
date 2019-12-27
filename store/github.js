cdbd67ac142928df9e528041f10bc0c3dbf1a8ea
yamanoku <0910yama@gmail.com>
2019-12-28 00:14:14 +0900

    add tokens

--- store/github.js
+++ store/github.js
@@ -1,3 +1,5 @@
+import { a11y_token, read_token, vue_token } from "../static/config";
+
 export const state = () => {
   return {
     a11y_issues: [],
@@ -20,19 +22,19 @@ export const mutations = {
 
 export const actions = {
   async fetchIssues (ctx) {
-    const a11y_url = `https://api.github.com/repos/yamanoku/accessibility_book-issues/issues`
+    const a11y_url = `https://api.github.com/repos/yamanoku/accessibility_book-issues/issues?access_token=${a11y_token}`
     const a11y_data = await this.$axios.get(a11y_url).then(response => {
       return response.data;
     });
     const a11y = a11y_data.filter(issue => !issue.pull_request);
     ctx.commit("SET_A11Y_ISSUES", a11y);
-    const read_url = `https://api.github.com/repos/yamanoku/reading/issues`
+    const read_url = `https://api.github.com/repos/yamanoku/reading/issues?access_token=${read_token}`
     const read_data = await this.$axios.get(read_url).then(response => {
       return response.data;
     });
     const reading = read_data.filter(issue => !issue.pull_request);
     ctx.commit("SET_READ_ISSUES", reading);
-    const vue_url = `https://api.github.com/repos/yamanoku/vue_portfolio_template/issues`
+    const vue_url = `https://api.github.com/repos/yamanoku/vue_portfolio_template/issues?access_token=${vue_token}`
     const vue_data = await this.$axios.get(vue_url).then(response => {
       return response.data;
     });
