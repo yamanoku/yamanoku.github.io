308a0fd0eee81a1c13ec0a3cb83c8886b6fc111a
yamanoku <0910yama@gmail.com>
2020-01-12 00:51:33 +0900

    chore: deploy [skip ci]

--- config.js
+++
@@ -1,4 +0,0 @@
-export const a11y_token = `acb7559596202e4023c349aef050de733ba5b8d4`;
-export const read_token = `ff750f23accf47d506438ca8d22a86887ef480d5`;
-export const vue_token = `a737a4d45acb5e29cc61f60b297094b994666e6b`;
-export const portfolio_token = `b0e1f94b652d3bc9d9a7bff5e4d476f039ca20d1`;
