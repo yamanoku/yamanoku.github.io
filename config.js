ba95b6fe03214d403b6566037ebe52310336b742
yamanoku <0910yama@gmail.com>
2020-01-12 00:41:25 +0900

    chore: deploy [skip ci]

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const a11y_token = `68f588badf61b6cd80925c0661edc6ca79d5a56a`
-export const read_token = `e5a0b95459ca4289231d2251024b275f1612c48e`
-export const vue_token = `5f8ce4db03063e0eb342056bcbb9dbff0cd17a41`
-export const portfolio_token = `657b7bdda158539992b91cafc2226b3aa5543746`
+export const a11y_token = `acb7559596202e4023c349aef050de733ba5b8d4`;
+export const read_token = `ff750f23accf47d506438ca8d22a86887ef480d5`;
+export const vue_token = `a737a4d45acb5e29cc61f60b297094b994666e6b`;
+export const portfolio_token = `b0e1f94b652d3bc9d9a7bff5e4d476f039ca20d1`;
--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw
 
 // Set workbox config
 workbox.setConfig({
-  "debug": false
+  "debug": true
 })
 
 // Start controlling any existing clients as soon as it activates
