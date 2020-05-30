3512165c58185fda5409c268981ac5b4d2fc37f7
yamanoku <0910yama@gmail.com>
2020-05-30 16:34:12 +0900

    update:components/about-articles/ComponentOriented.vue

--- components/about-articles/ComponentOriented.vue
+++ components/about-articles/ComponentOriented.vue
@@ -7,38 +7,41 @@
       {{ $t("aboutPage.subHeading.components") }}
     </h3>
     <p>{{ $t("aboutPage.components.desc01") }}</p>
-    <figure>
-      <highlight-code id="slide-components" lang="vue">{{
-        slide
-      }}</highlight-code>
-      <figcaption>
-        <a
-          href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/lists/SlideList.vue#L1-L18"
-          target="_blank"
-          rel="noopener"
-        >
-          SlideList.vue
-          <github-icon />
-        </a>
-      </figcaption>
-    </figure>
-    <p>{{ $t("aboutPage.components.desc02") }}</p>
-    <p>{{ $t("aboutPage.components.desc03") }}</p>
-    <figure>
-      <highlight-code id="api-for-store" lang="javascript">{{
-        api
-      }}</highlight-code>
-      <figcaption>
-        <a
-          href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/products/A11yBeginner.vue#L52-L58"
-          target="_blank"
-          rel="noopener"
-        >
-          A11yBeginner.vue
-          <github-icon />
-        </a>
-      </figcaption>
-    </figure>
+    <details>
+      <summary>{{ $t("details.summary") }}</summary>
+      <p>{{ $t("aboutPage.components.desc02") }}</p>
+      <figure>
+        <highlight-code id="slide-components" lang="vue">{{
+          slide
+        }}</highlight-code>
+        <figcaption>
+          <a
+            href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/lists/SlideList.vue#L1-L18"
+            target="_blank"
+            rel="noopener"
+          >
+            SlideList.vue
+            <github-icon />
+          </a>
+        </figcaption>
+      </figure>
+      <p>{{ $t("aboutPage.components.desc03") }}</p>
+      <figure>
+        <highlight-code id="api-for-store" lang="javascript">{{
+          api
+        }}</highlight-code>
+        <figcaption>
+          <a
+            href="https://github.com/yamanoku/yamanoku.github.io/blob/nuxt/components/products/A11yBeginner.vue#L52-L58"
+            target="_blank"
+            rel="noopener"
+          >
+            A11yBeginner.vue
+            <github-icon />
+          </a>
+        </figcaption>
+      </figure>
+    </details>
   </article>
 </template>
 
