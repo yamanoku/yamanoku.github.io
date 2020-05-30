5a8d1c50d33888b7da900e46159bf4cb72b0d38c
yamanoku <0910yama@gmail.com>
2020-05-30 17:10:08 +0900

    update:components/about-articles/TechStack.vue

--- components/about-articles/TechStack.vue
+++ components/about-articles/TechStack.vue
@@ -3,19 +3,25 @@
     <h3 id="about:tech-stack_heading">
       {{ $t("heading.techStack") }}
     </h3>
-    <ul role="list">
+    <ul role="list" lang="en">
       <li role="listitem">
         Nuxt.js
         <ul role="list">
-          <li role="listitem">@nuxtjs/pwa</li>
-          <li role="listitem">@nuxtjs/google-gtag</li>
           <li role="listitem">@nuxtjs/axios</li>
+          <li role="listitem">@nuxtjs/dotenv</li>
+          <li role="listitem">@nuxtjs/pwa</li>
           <li role="listitem">nuxt-i18n</li>
-          <li role="listitem">eslint-plugin-vue</li>
-          <li role="listitem">eslint-plugin-vue-a11y</li>
         </ul>
       </li>
       <li role="listitem">Vuex （Store）</li>
+      <li role="listitem">
+        eslint
+        <ul role="list">
+          <li role="listitem">eslint-plugin-prettier</li>
+          <li role="listitem">eslint-plugin-vue</li>
+          <li role="listitem">eslint-plugin-vuejs-accessibility</li>
+        </ul>
+      </li>
       <li role="listitem">modern-normalize</li>
       <li role="listitem">Google Fonts</li>
     </ul>
