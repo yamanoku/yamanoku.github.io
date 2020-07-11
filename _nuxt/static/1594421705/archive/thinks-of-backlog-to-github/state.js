f26b6e0204adbd45d47a3c72e0717ffb982f2054
yamanoku <yamanoku@users.noreply.github.com>
2020-07-11 02:17:10 +0000

    Automated deployment: Sat Jul 11 02:17:10 UTC 2020 57bd887a3fd870f6b8a31048823c760077b44850

--- _nuxt/static/1594421705/archive/thinks-of-backlog-to-github/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1594421705",layout:"default",error:null,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Farchive\u002Fthinks-of-backlog-to-github",config:{}};
