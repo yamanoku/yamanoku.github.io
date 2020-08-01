506fce5675117deae72bf9f9dc9cc4fe1f0b9eed
yamanoku <yamanoku@users.noreply.github.com>
2020-08-01 01:24:08 +0000

    Automated deployment: Sat Aug  1 01:24:08 UTC 2020 b4e935d66d88667807753332cedbed4b559dc9dd

---
+++ _nuxt/static/1596245038/archive/wordpress-and-sectioning-markup/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1596245038",layout:"default",error:null,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Farchive\u002Fwordpress-and-sectioning-markup",config:{}};
