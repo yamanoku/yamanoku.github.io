b57a6e405b2eec0fa50b81183c84e4560c633d2b
yamanoku <0910yama@gmail.com>
2020-09-21 18:50:18 +0900

    add:Donate section

---
+++ components/sections/Donate.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="donate" aria-labelledby="donate_heading">
+    <h2 id="donate_heading">
+      {{ $t("heading.donate") }}
+    </h2>
+    <p>
+      {{ $t("donate.desc01") }}
+    </p>
+    <ul role="list">
+      <li role="listitem">
+        <a
+          target="_blank"
+          rel="noopener"
+          href="http://buymeacoffee.com/yamanoku"
+          lang="en"
+        >
+          <span role="img" aria-label="beer">🍺</span>
+          Buy me a Beer
+          <open-new-icon />
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
