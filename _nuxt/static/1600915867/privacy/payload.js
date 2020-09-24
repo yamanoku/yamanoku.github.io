2d50d91352f6c1aadc5f2ee4eeb051d21b84c484
yamanoku <yamanoku@users.noreply.github.com>
2020-09-24 02:51:19 +0000

    Automated deployment: Thu Sep 24 02:51:19 UTC 2020 4c525558d2452b4992c53b58970d7f56968cdf24

---
+++ _nuxt/static/1600915867/privacy/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/privacy", (function(a){return {data:[{baseName:a,baseDesc:"Okuto Oyama Portfolio Site",baseUrl:"https:\u002F\u002Fyamanaoku.net",baseOgp:"https:\u002F\u002Fyamanoku.net\u002Fogp\u002Fogp@2x.png",baseOgpAlt:a}],fetch:[],mutations:[]}}("yamanoku.net")));
