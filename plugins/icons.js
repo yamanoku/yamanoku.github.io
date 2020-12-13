b46c991f32a4e0c6e466c73c54d3b4075ef79964
yamanoku <0910yama@gmail.com>
2020-12-13 15:52:43 +0900

    del:unused icons

--- plugins/icons.js
+++ plugins/icons.js
@@ -1,18 +1,12 @@
 import Vue from "vue";
 
 import GithubIcon from "~/assets/svg-icons/GithubIcon.vue";
-import NpmIcon from "~/assets/svg-icons/NpmIcon.vue";
-import AnchorIcon from "~/assets/svg-icons/AnchorIcon.vue";
-import ProductIcon from "~/assets/svg-icons/ProductIcon.vue";
 import ImageIcon from "~/assets/svg-icons/ImageIcon.vue";
 import ShoppingIcon from "~/assets/svg-icons/ShoppingIcon.vue";
 import OpenNewIcon from "~/assets/svg-icons/OpenNewIcon.vue";
 import MailIcon from "~/assets/svg-icons/MailIcon.vue";
 
 Vue.component("github-icon", GithubIcon);
-Vue.component("npm-icon", NpmIcon);
-Vue.component("anchor-icon", AnchorIcon);
-Vue.component("product-icon", ProductIcon);
 Vue.component("image-icon", ImageIcon);
 Vue.component("shopping-icon", ShoppingIcon);
 Vue.component("open-new-icon", OpenNewIcon);
