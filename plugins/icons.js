abb933ab7064570410c08035af4f7258e92f142b
yamanoku <0910yama@gmail.com>
2021-07-25 14:05:59 +0900

    del: image icon

--- plugins/icons.js
+++ plugins/icons.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 
 import GithubIcon from "~/assets/svg-icons/GithubIcon.vue";
-import ImageIcon from "~/assets/svg-icons/ImageIcon.vue";
 import ShoppingIcon from "~/assets/svg-icons/ShoppingIcon.vue";
 import OpenNewIcon from "~/assets/svg-icons/OpenNewIcon.vue";
 import MailIcon from "~/assets/svg-icons/MailIcon.vue";
 
 Vue.component("github-icon", GithubIcon);
-Vue.component("image-icon", ImageIcon);
 Vue.component("shopping-icon", ShoppingIcon);
 Vue.component("open-new-icon", OpenNewIcon);
 Vue.component("mail-icon", MailIcon);
