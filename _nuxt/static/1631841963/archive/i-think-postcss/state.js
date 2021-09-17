48e013da27c922a7761bcd4df7cce7af15d79cab
yamanoku <yamanoku@users.noreply.github.com>
2021-09-17 01:26:13 +0000

    deploy: 3da5ab82e4288b83ff8bd254ed8da5d1c9fd5ebe

---
+++ _nuxt/static/1631841963/archive/i-think-postcss/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631841963",layout:"default",error:a,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Farchive\u002Fi-think-postcss",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6bad3887"}},__i18n:{langs:{}}}}(null));
