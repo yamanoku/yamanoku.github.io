13866ddd203ef7df83bbd4b531a633d6327382d0
yamanoku <yamanoku@users.noreply.github.com>
2021-09-17 01:26:22 +0000

    deploy: 390e5764dc7f1d2ec9f82f8faa0a673253fe2571

---
+++ _nuxt/static/1631841974/archive/cookie-with-javascript/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631841974",layout:"default",error:a,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Farchive\u002Fcookie-with-javascript",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d02e87b2"}},__i18n:{langs:{}}}}(null));
