d55242666f32a3121af31576e8a117f4853a7bd4
yamanoku <yamanoku@users.noreply.github.com>
2021-09-18 10:12:34 +0000

    deploy: a871b166da9384780b4f83dcb20610d19b2a9593

--- _nuxt/static/1631919027/archive/techbookfest-04/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631919027",layout:"default",error:a,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Farchive\u002Ftechbookfest-04",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"336ced0d"}},__i18n:{langs:{}}}}(null));
