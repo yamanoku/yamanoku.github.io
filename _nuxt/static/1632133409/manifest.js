9cf95f890ad33aa5951358132bf7fe5b0d8322ab
yamanoku <yamanoku@users.noreply.github.com>
2021-09-20 11:26:12 +0000

    deploy: c47e9d817a8258bfd3bacfd3f73a2d1eb197c613

--- _nuxt/static/1632133409/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fabout","\u002Fabout","\u002Fprivacy","\u002F404","\u002Fvue-a11y","\u002Fen\u002Fprivacy","\u002Fen\u002Fvue-a11y","\u002Fen"]})
---
+++ _nuxt/static/1632137169/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fabout","\u002F404","\u002Fabout","\u002Fprivacy","\u002Fvue-a11y","\u002Fvue-a11y-2019","\u002Fen\u002Fprivacy","\u002Fen\u002Fvue-a11y","\u002Fen\u002Fvue-a11y-2019","\u002Fen"]})
