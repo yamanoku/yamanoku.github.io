77bbbca80531f0580955836baf638f204d706a56
yamanoku <0910yama@gmail.com>
2021-09-21 19:29:53 +0900

    del: about pages components

--- components/about-sections/MaxWidth.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section id="about:max-width" aria-labelledby="about:max-width_heading">
-    <h3 id="about:max-width_heading">
-      {{ $t("aboutPage.subHeading.maxWidth") }}
-    </h3>
-    <p>{{ $t("aboutPage.maxWidth.desc01") }}</p>
-    <p>{{ $t("aboutPage.maxWidth.desc02") }}</p>
-  </section>
-</template>
