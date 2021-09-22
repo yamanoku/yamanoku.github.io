ab6be73fff59729e54b3b10a2fbda7ce55b795a4
yamanoku <yamanoku@users.noreply.github.com>
2021-09-22 06:43:33 +0000

    deploy: 6ea4dafb292eec45b35251e602afca6c1c2f629d

--- _nuxt/static/1632271090/404/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632271090",layout:"default",error:a,state:{rhythm:{toggleRhythm:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002F404",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
