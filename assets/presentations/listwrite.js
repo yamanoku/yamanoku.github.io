4723fc8283d2eed864f333405858dffa87b5aa39
yamanoku <0910yama@gmail.com>
2021-12-18 23:08:40 +0900

    update: listWrite

--- assets/presentations/listwrite.js
+++ assets/presentations/listwrite.js
@@ -1,5 +1,15 @@
 export const listWrite = [
   {
+    title: "クラウドワークス #accessibility チャンネル で気になった話題 Pickup 2021年7〜12月編",
+    url: "https://zenn.dev/yamanoku/scraps/d964d3efc75e09",
+    datetime: "2021-12-14T00:00:00.000Z",
+  },
+  {
+    title: "クラウドワークス #accessibility チャンネル で気になった話題 Pickup 2021年1〜6月編",
+    url: "https://zenn.dev/yamanoku/scraps/6e4f4d5ed7563a",
+    datetime: "2021-12-14T00:00:00.000Z",
+  },
+  {
     title: "crowdworks.jp のフロントエンド活動を振り返る 2021",
     url: "https://qiita.com/yamanoku/items/29a74ebf3d74b3017581",
     datetime: "2021-12-01T07:00:00.000Z",
@@ -13,16 +23,5 @@ export const listWrite = [
     title: "生まれ変わったログインページにまつわるフロントエンド開発の話",
     url: "https://engineer.crowdworks.jp/entry/renewal-login",
     datetime: "2021-08-18T18:00:00.000Z",
-  },
-  {
-    title: "UI の実装で考えていること, 気をつけたいこと",
-    url: "https://zenn.dev/yamanoku/articles/c69decedfb1699",
-    datetime: "2020-12-09T18:00:00.000Z",
-  },
-  {
-    title:
-      "クラウドワークス #accessibility チャンネル 気になる話題 Pickup 2020",
-    url: "https://zenn.dev/yamanoku/scraps/443087a170fcbe",
-    datetime: "2020-12-07T00:00:00.000Z",
   },
 ];
