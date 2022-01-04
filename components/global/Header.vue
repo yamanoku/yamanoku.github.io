734c71cf4be2de4942c56bc74494a95ca90b670c
yamanoku <0910yama@gmail.com>
2022-01-04 21:16:29 +0900

    update: svg label, title

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -2,14 +2,13 @@
   <header>
     <nuxt-link :to="localePath({ name: 'index' })">
       <svg
-        xmlns="http://www.w3.org/2000/svg"
         width="246"
         height="242"
         viewBox="0 0 246 242"
         role="img"
-        aria-labelledby="title"
+        aria-label="yamanoku.net"
       >
-        <title id="title">yamanoku.net</title>
+        <title>{{ $t("home.backToLink") }}</title>
         <path
           class="cls-1"
           d="M64,67v54l82,82-46,46v60h56L310,155V96H230l-21,20L160,67H64ZM176,203l-45,46h25L293,113H230l-39,39-31-31H94Z"
