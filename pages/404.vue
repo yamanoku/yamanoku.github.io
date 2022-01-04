9edec4e3ee8bcef6b4a3911af6c30487a5735caa
yamanoku <0910yama@gmail.com>
2022-01-04 20:22:39 +0900

    fix: eslint

--- pages/404.vue
+++ pages/404.vue
@@ -3,7 +3,8 @@
     <h1>ページが見つかりませんでした</h1>
     <p>
       申し訳ありません。お探しのページが見つかりませんでした。<br />
-      お手数をおかけしますが、<nuxt-link to="/">ホーム</nuxt-link>にお戻りください。
+      お手数をおかけしますが、<nuxt-link to="/">ホーム</nuxt-link
+      >にお戻りください。
     </p>
     <p lang="en">
       Sorry. Couldn't find the page you were looking for.<br />
@@ -15,7 +16,7 @@
 
 <script>
 export default {
-  name: "Page404",
+  name: "NotFoundPage",
   head() {
     return {
       title: "ページが見つかりませんでした"
