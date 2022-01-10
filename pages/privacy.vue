8408e1ff84a659716a5869c5504a1f1abc0a0651
yamanoku <0910yama@gmail.com>
2022-01-10 12:39:56 +0900

    privacy components

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,34 +1,8 @@
 <template>
   <main id="main">
     <h1 id="privacy-policy_heading">{{ $t("privacyPage.title") }}</h1>
-    <section id="privacy:analytics" aria-labelledby="privacy:analytics_heading">
-      <h2 id="privacy:analytics_heading">
-        {{ $t("privacyPage.analytics.heading") }}
-      </h2>
-      <p>{{ $t("privacyPage.analytics.desc01") }}</p>
-      <p>{{ $t("privacyPage.analytics.desc02") }}</p>
-      <p>{{ $t("privacyPage.analytics.desc03") }}</p>
-      <h3 id="related:analytics_heading">{{ $t("heading.relatedList") }}</h3>
-      <ul aria-labelledby="related:analytics_heading">
-        <li>
-          <a
-            :href="$t('privacyPage.analytics.link01.url')"
-            target="_blank"
-            rel="noopener"
-            >{{ $t("privacyPage.analytics.link01.title") }}<external-link-icon
-          /></a>
-        </li>
-        <li>
-          <a
-            :href="$t('privacyPage.analytics.link02.url')"
-            target="_blank"
-            rel="noopener"
-            >{{ $t("privacyPage.analytics.link02.title") }}<external-link-icon
-          /></a>
-        </li>
-      </ul>
-    </section>
-    <GlobalLocalSwitch />
+    <privacy-analytics />
+    <global-local-switch />
   </main>
 </template>
 
@@ -44,9 +18,9 @@ export default {
         title: this.$t("privacyPage.title"),
         description: this.$t("privacyPage.description"),
         image: "https://yamanoku.net/ogp/ogp-privacy@2x.png",
-        imageAlt: this.$t("privacyPage.title")
-      }
+        imageAlt: this.$t("privacyPage.title"),
+      },
     };
-  }
+  },
 };
 </script>
