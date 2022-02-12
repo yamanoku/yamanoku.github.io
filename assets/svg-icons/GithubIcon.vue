a935f7be50de4967d97299a40d18cc106c073c58
yamanoku <0910yama@gmail.com>
2022-02-12 13:37:51 +0900

    fix: ineffective-attr

--- assets/svg-icons/GithubIcon.vue
+++ assets/svg-icons/GithubIcon.vue
@@ -4,7 +4,6 @@
     width="12"
     height="12"
     viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
   >
     <title>{{ $t("iconTitle.github") }}</title>
     <path
--- assets/svg-icons/MailIcon.vue
+++ assets/svg-icons/MailIcon.vue
@@ -1,7 +1,6 @@
 <template>
   <svg
     role="img"
-    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
