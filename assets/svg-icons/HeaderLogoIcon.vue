67bc5eb94cdf28d9ba378c6af2227c1c6ba10bd2
yamanoku <0910yama@gmail.com>
2022-04-29 15:32:01 +0900

    update: header-logo

--- assets/svg-icons/HeaderLogoIcon.vue
+++ assets/svg-icons/HeaderLogoIcon.vue
@@ -6,7 +6,6 @@
     role="img"
     aria-label="yamanoku logo"
   >
-    <title>{{ $t("backHome") }}</title>
     <path
       d="M64,67v54l82,82-46,46v60h56L310,155V96H230l-21,20L160,67H64ZM176,203l-45,46h25L293,113H230l-39,39-31-31H94Z"
       style="transform: translate(-64px, -67px)"
