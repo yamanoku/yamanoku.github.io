d5392c3fa63746adfca09528f4f36234b7aba4b7
yamanoku <0910yama@gmail.com>
2022-11-06 20:05:49 +0900

    update basic info markup

--- components/pages/index/sections/BasicInfo.vue
+++ components/pages/index/sections/BasicInfo.vue
@@ -5,12 +5,33 @@
       :heading-level="2"
       :heading-text="$t('heading.basic')"
     />
-    <template v-if="this.$i18n.locale === 'ja'">
-      大山奥人
-      <small>（おおやまおくと）</small>
-      。{{ $t("info.caption") }}
-    </template>
-    <template v-else>Okuto Oyama. {{ $t("info.caption") }}</template>
+    <dl>
+      <dt>{{ this.$t("info.realName.key") }}</dt>
+      <dd>
+        <template v-if="this.$i18n.locale === 'ja'">
+          <ruby
+            >大<rp>(</rp><rt>おお</rt><rp>)</rp>山<rp>(</rp><rt>やま</rt
+            ><rp>)</rp></ruby
+          ><ruby
+            >奥<rp>(</rp><rt>おく</rt><rp>)</rp>人<rp>(</rp><rt>と</rt
+            ><rp>)</rp></ruby
+          >
+        </template>
+        <template v-else>Okuto Oyama</template>
+      </dd>
+      <dt>{{ this.$t("info.hundleName.key") }}</dt>
+      <dd>{{ this.$t("info.hundleName.value") }}</dd>
+      <dt>{{ this.$t("info.birthday.key") }}</dt>
+      <dd>{{ this.$t("info.birthday.value") }}</dd>
+      <dt>{{ this.$t("info.sex.key") }}</dt>
+      <dd>{{ this.$t("info.sex.value") }}</dd>
+      <dt>{{ this.$t("info.birthplace.key") }}</dt>
+      <dd>{{ this.$t("info.birthplace.value") }}</dd>
+      <dt>{{ this.$t("info.location.key") }}</dt>
+      <dd>{{ this.$t("info.location.value") }}</dd>
+      <dt>{{ this.$t("info.family.key") }}</dt>
+      <dd>{{ this.$t("info.family.value") }}</dd>
+    </dl>
   </section>
 </template>
 
