39b66c096e8ff07b83590ef48d31af998f4a92f3
yamanoku <0910yama@gmail.com>
2022-12-22 08:05:04 +0900

    update

--- assets/presentations/listwrite.js
+++ assets/presentations/listwrite.js
@@ -1,11 +1,23 @@
 export const listWrite = [
   {
+    title: "ありがとう Webpacker さようなら Webpacker",
+    url: "https://engineer.crowdworks.jp/entry/thanks_webpacker_goodbye_webpacker",
+    datetime: "2022-12-13T12:00:00.000Z",
+    isExternal: true
+  },
+  {
+    title:
+      "クラウドワークス #accessibility チャンネル 気になる話題 Pickup 2022",
+    url: "https://zenn.dev/yamanoku/scraps/afe08c2634034c",
+    datetime: "2022-12-12T00:00:00.000Z",
+    isExternal: true
+  },
+  {
     title: "crowdworks.jp のフロントエンド活動を振り返る 2022",
     url: "https://engineer.crowdworks.jp/entry/crowdworks_frontend_2022",
     datetime: "2022-12-01T12:00:00.000Z",
     isExternal: true
   },
-
   {
     title: "Vue Fes Japan Online 2022 参加レポート",
     url: "https://engineer.crowdworks.jp/entry/report-vue-fes-japan-online-2022",
@@ -28,20 +40,6 @@ export const listWrite = [
     title: "2021年オープンソースコントリビュート活動振り返り",
     url: "https://zenn.dev/yamanoku/articles/bd8d86ee79459b",
     datetime: "2021-12-30T10:00:00.000Z",
-    isExternal: true
-  },
-  {
-    title:
-      "クラウドワークス #accessibility チャンネル で気になった話題 Pickup 2021年7〜12月編",
-    url: "https://zenn.dev/yamanoku/scraps/d964d3efc75e09",
-    datetime: "2021-12-14T00:00:00.000Z",
-    isExternal: true
-  },
-  {
-    title:
-      "クラウドワークス #accessibility チャンネル で気になった話題 Pickup 2021年1〜6月編",
-    url: "https://zenn.dev/yamanoku/scraps/6e4f4d5ed7563a",
-    datetime: "2021-12-14T00:00:00.000Z",
     isExternal: true
   }
 ];
