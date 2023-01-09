19b05cc1841ea38a4c79d85fc8f057c1ca05c9a6
yamanoku <0910yama@gmail.com>
2023-01-09 22:07:18 +0900

    add: src/i18n

---
+++ src/i18n/ja/dictionary.ts
@@ -0,0 +1,47 @@
+export default {
+  // meta 情報
+  "meta.title": "ホーム | yamanoku.net",
+  "meta.description": "大山奥人のポートフォリオサイト",
+
+  // 見出し
+  "heading.basic": "基本情報",
+  "heading.jobs": "職業",
+  "heading.career": "職歴",
+  "heading.related-list": "関連リンク一覧",
+  "heading.presentations.title": "発表一覧",
+  "heading.presentations.stage": "登壇",
+  "heading.presentations.write": "執筆",
+  "heading.sns": "ソーシャルサービス",
+  "heading.donate": "寄付・支援",
+  "heading.contact": "連絡先",
+  "heading.open-source-activity": "オープンソース活動",
+
+  // 基本情報
+  "info.real-name": "本名",
+  "info.hundle-name": "ハンドルネーム",
+  "info.birthday": "誕生日",
+  "info.sex": "性別",
+  "info.birthplace": "出身",
+  "info.location": "所在",
+  "info.family": "家族構成",
+
+  // 職歴
+  "career.jobs.frontend-designer": "フロントエンドデザイナー",
+  "career.jobs.ui-designer": "UIデザイナー",
+  "career.links.curriculum-vitae": "職務経歴書",
+  "career.links.wantedly": "Wantedly",
+  "career.links.lapras": "LAPRAS",
+  "career.links.youtrust": "YOUTRUST",
+
+  // オープンソース活動
+  "open-source-activity.descripion": "企業に所属して働く傍ら、余暇時間を使って2020年よりオープンソースへのコントリビュート活動をしています。ドキュメントの翻訳やアクセシビリティ改善提案や修正対応、その他軽微な修正などを行っています。",
+  "open-source-activity.link-2021": "オープンソース活動記録 2021年",
+
+  // 寄付・支援
+  "donate.description": "もし私の制作物や発表、活動に興味をもっていただけた場合以下より支援することができます。",
+
+  // アイコン
+  "icon-title.github": "GitHub に移動する",
+  "icon-title.mail": "メールアプリを立ち上げる",
+  "icon-title.external-link": "外部サイトに移動する",
+};
---
+++ src/i18n/languages.ts
@@ -0,0 +1,8 @@
+/**
+ * Map of language codes to a written out language name.
+ * Used to populate the language switcher in the navbar.
+ */
+export default {
+  en: "English",
+  ja: "日本語",
+};
