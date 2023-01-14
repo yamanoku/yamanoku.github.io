c81f08c8e1a502a8223cd0f3f7f1022988fc13b7
yamanoku <0910yama@gmail.com>
2023-01-14 22:41:54 +0900

    remove: settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,18 +11,9 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "plugin:vuejs-accessibility/recommended",
-    "plugin:prettier/recommended"
   ],
   plugins: ["vue", "vuejs-accessibility"],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        semi: true,
-        trailingComma: "none",
-        arrowParens: "avoid"
-      }
-    ],
     "vuejs-accessibility/no-redundant-roles": 0,
     "vue/no-template-target-blank": [
       "error",
