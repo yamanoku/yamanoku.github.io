1ab2d78a22a75373647610f8b37b591ad3c1b546
yamanoku <0910yama@gmail.com>
2023-05-06 02:06:23 +0900

    update

--- src/presentations/listPresentation.ts
+++ src/presentations/listPresentation.ts
@@ -58,11 +58,5 @@ export const listWrite: ListType[] = [
     url: "https://engineer.crowdworks.jp/entry/report-vue-fes-japan-online-2022",
     datetime: "2022-10-25T03:00:00.000Z",
     isExternal: true
-  },
-  {
-    title: "「HTML解体新書」HTMLのこれからと向き合うための本",
-    url: "https://engineer.crowdworks.jp/entry/html-anatomische-tabell",
-    datetime: "2022-04-19T17:00:00.000Z",
-    isExternal: true
   }
 ];
