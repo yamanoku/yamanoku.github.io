951e620a79ede6c57d6a1d8753a29bde4703ff81
yamanoku <0910yama@gmail.com>
2025-02-08 19:26:03 +0900

    add: 分散型SNSで生活してみての所感〜周辺動向と鯖引越しを添えて〜

--- src/presentations/listPresentation.ts
+++ src/presentations/listPresentation.ts
@@ -10,6 +10,11 @@ type ExactPresantationLengthArray<T> = {
 
 export const listStage: ExactPresantationLengthArray<ListType> = [
   {
+    title: "分散型SNSで生活してみての所感〜周辺動向と鯖引越しを添えて〜",
+    url: "https://scrapbox.io/yamanoku/%E5%88%86%E6%95%A3%E5%9E%8BSNS%E3%81%A7%E7%94%9F%E6%B4%BB%E3%81%97%E3%81%A6%E3%81%BF%E3%81%A6%E3%81%AE%E6%89%80%E6%84%9F%E3%80%9C%E5%91%A8%E8%BE%BA%E5%8B%95%E5%90%91%E3%81%A8%E9%AF%96%E5%BC%95%E8%B6%8A%E3%81%97%E3%82%92%E6%B7%BB%E3%81%88%E3%81%A6%E3%80%9C",
+    datetime: "2025-02-08T:00:00.00+09:00"
+  },
+  {
     title: "動画にまつわるアクセシビリティ要件を探る",
     url: "https://scrapbox.io/yamanoku/%E5%8B%95%E7%94%BB%E3%81%AB%E3%81%BE%E3%81%A4%E3%82%8F%E3%82%8B%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B7%E3%83%93%E3%83%AA%E3%83%86%E3%82%A3%E8%A6%81%E4%BB%B6%E3%82%92%E6%8E%A2%E3%82%8B",
     datetime: "2024-11-29T19:54:00.00+09:00"
@@ -29,11 +34,6 @@ export const listStage: ExactPresantationLengthArray<ListType> = [
     title: "令和最新静的サイトジェネレーター事情",
     url: "https://scrapbox.io/yamanoku/%E4%BB%A4%E5%92%8C%E6%9C%80%E6%96%B0%E9%9D%99%E7%9A%84%E3%82%B5%E3%82%A4%E3%83%88%E3%82%B8%E3%82%A7%E3%83%8D%E3%83%AC%E3%83%BC%E3%82%BF%E3%83%BC%E4%BA%8B%E6%83%85",
     datetime: "2024-04-25T10:30:00.00+09:00"
-  },
-  {
-    title: "Svelte Accessibility Warning",
-    url: "https://docs.google.com/presentation/d/e/2PACX-1vQ8UFLM2Rxnx-pf8ZI9SgU5ZKY5hPpLQa060LHDQ9fu4ddHVPJO_ECDOomNlSp6a0PyeL6UNc36kt6r/pub?slide=id.p1",
-    datetime: "2024-02-22T11:30:00.00+09:00"
   }
 ];
 
