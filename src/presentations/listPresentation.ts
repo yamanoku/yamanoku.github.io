1a2c68f7e19f70043b2052b7c7422506289010d2
yamanoku <0910yama@gmail.com>
2025-05-18 08:23:33 +0900

    add: React 18からのAPI useIDを使おう！

--- src/presentations/listPresentation.ts
+++ src/presentations/listPresentation.ts
@@ -10,6 +10,11 @@ type ExactPresantationLengthArray<T> = {
 
 export const listStage: ExactPresantationLengthArray<ListType> = [
   {
+    title: "React 18からのAPI useIDを使おう！",
+    url: "https://speakerdeck.com/yamanoku/lets-use-the-useid-api-from-react-18",
+    datetime: "2025-05-17T16:00:00.00+09:00"
+  },
+  {
     title: "リアクティブシステムの変遷から理解するalien-signals",
     url: "https://speakerdeck.com/yamanoku/learning-alien-signals-from-the-evolution-of-reactive-systems",
     datetime: "2025-03-28T19:20:00.00+09:00"
@@ -28,12 +33,6 @@ export const listStage: ExactPresantationLengthArray<ListType> = [
     title: "動画にまつわるアクセシビリティ要件を探る",
     url: "https://scrapbox.io/yamanoku/%E5%8B%95%E7%94%BB%E3%81%AB%E3%81%BE%E3%81%A4%E3%82%8F%E3%82%8B%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B7%E3%83%93%E3%83%AA%E3%83%86%E3%82%A3%E8%A6%81%E4%BB%B6%E3%82%92%E6%8E%A2%E3%82%8B",
     datetime: "2024-11-29T19:54:00.00+09:00"
-  },
-  {
-    title:
-      "ブラウザ互換の重要性 - あらゆるユーザーに価値を届けるために必要なこと",
-    url: "https://yamanoku.net/frontendo-2024/ja/",
-    datetime: "2024-08-23T04:20:00.00+09:00"
   }
 ];
 
