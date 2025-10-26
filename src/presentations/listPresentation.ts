d0a7500ed0d37730dcd56762e34dc17262855374
yamanoku <0910yama@gmail.com>
2025-10-26 16:18:15 +0900

    update

--- src/presentations/listPresentation.ts
+++ src/presentations/listPresentation.ts
@@ -10,6 +10,16 @@ type ExactPresantationLengthArray<T> = {
 
 export const listStage: ExactPresantationLengthArray<ListType> = [
   {
+    title: "生成AI時代のWebアプリケーションアクセシビリティ改善",
+    url: "https://yamanoku.net/vuefes-japan-2025/ja/",
+    datetime: "2025-10-25T12:50:00.00+09:00"
+  },
+  {
+    title: "Svelte Accessibility on 2025-10-24",
+    url: "https://scrapbox.io/yamanoku/Svelte_Accessibility_on_2025-10-24",
+    datetime: "2025-10-24T20:39:00.00+09:00"
+  },
+  {
     title: "改めてカルーセルを見つめ直してみる Summer 2025",
     url: "https://scrapbox.io/yamanoku/%E6%94%B9%E3%82%81%E3%81%A6%E3%82%AB%E3%83%AB%E3%83%BC%E3%82%BB%E3%83%AB%E3%82%92%E8%A6%8B%E3%81%A4%E3%82%81%E7%9B%B4%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B_Summer_2025",
     datetime: "2025-09-08T20:03:00.00+09:00"
@@ -23,17 +33,6 @@ export const listStage: ExactPresantationLengthArray<ListType> = [
     title: "Proposal for ECMAScript enumsについて（2025-06-06時点）",
     url: "https://scrapbox.io/yamanoku/Proposal_for_ECMAScript_enums%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6%EF%BC%882025-06-06%E6%99%82%E7%82%B9%EF%BC%89",
     datetime: "2025-06-06T19:48:00.00+09:00"
-  },
-  {
-    title:
-      "転生したら​TypeScriptの​Enumだった​件～型​安全性と​エコシステムの​変化で​挫けそうになっているんだが​～",
-    url: "https://yamanoku.net/tskaigi-2025/",
-    datetime: "2025-05-23T15:00:00.00+09:00"
-  },
-  {
-    title: "React 18からのAPI useIDを使おう！",
-    url: "https://speakerdeck.com/yamanoku/lets-use-the-useid-api-from-react-18",
-    datetime: "2025-05-17T16:00:00.00+09:00"
   }
 ];
 
